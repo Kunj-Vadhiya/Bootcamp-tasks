25485a2f4dc7b09170100358d018f91320592391
Kunj Vadhiya <kunjvadhiya19@gmail.com>
2025-08-11 20:51:23 +0530

    Changed

--- Project/script.js
+++ Project/script.js
@@ -20,82 +20,16 @@ function deleteLast()
     display.value = display.value.slice(0, -1);
 }
 
-function toRad(degree)
-{
-    return degree*(Math.PI/180);
-}
-
-// function sinDeg()
-// {
-//     if(display.value !== "")
-//     {
-//         let value = parseFloat(display.value);
-//         let result = Math.sin(toRad(value));
-//         display.value = Number(result.toFixed(10));
-//     }
-// }
-
-// function cosDeg()
-// {
-//     if(display.value !== "")
-//     {
-//         let value = parseFloat(display.value);
-//         let result = Math.cos(toRad(value));
-//         display.value = Number(result.toFixed(10));
-//     }
-// }
-
-// function tanDeg()
-// {
-//     if(display.value !== "")
-//     {
-//         let value = parseFloat(display.value);
-//         let result = Math.tan(toRad(value));
-//         display.value = Number(result.toFixed(10));
-//     }
-// }
-
-function sin(x)
-{
-    return Number(Math.sin(toRad(x)).toFixed(10));
-}
-
-
-function cos(x)
-{
-    return Number(Math.cos(toRad(x)).toFixed(10));
-}
-
-
-function tan(x)
-{
-    return Number(Math.tan(toRad(x)).toFixed(10));
-}
-
-function calculate() 
-{
-    try {
-        let expression = display.value.replace(/\^/g, "**");
-        if (expression.includes("!")) {
-        expression = expression.replace(/(\d)!/g, (_, n) => factorial(parseInt(n)));
-    }
-        display.value = eval(expression);
-    } catch {
-        display.value = "Error";
-    }
-}
-// 
-
 function factorial(n) 
 {
     return n <= 1 ? 1 : n * factorial(n - 1);
 }
 
-function toggleScientific() 
+function calculate()
 {
-    const sci = document.getElementById("sci");
-    sci.classList.toggle("show");
-
-    const toggleBtn = document.querySelector(".toggle-btn");
-    toggleBtn.innerText = sci.classList.contains("show") ? "▼ Other Operations": "▶ Other Operations";
+    try{
+        display.value = eval(display.value);
+    }catch{
+        display.value = "Error";
+    }
 }

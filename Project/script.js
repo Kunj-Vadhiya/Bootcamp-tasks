8b04b989ec191a583575dde39faa4e87278edcac
Kunj Vadhiya <kunjvadhiya19@gmail.com>
2025-08-12 18:51:35 +0530

    Final

--- Project/script.js
+++ Project/script.js
@@ -20,16 +20,51 @@ function deleteLast()
     display.value = display.value.slice(0, -1);
 }
 
+function toRad(degree)
+{
+    return degree*(Math.PI/180);
+}
+
+function sin(x)
+{
+    return Number(Math.sin(toRad(x)).toFixed(10));
+}
+
+
+function cos(x)
+{
+    return Number(Math.cos(toRad(x)).toFixed(10));
+}
+
+
+function tan(x)
+{
+    return Number(Math.tan(toRad(x)).toFixed(10));
+}
+
+function calculate() 
+{
+    try {
+        let expression = display.value.replace(/\^/g, "**");
+        if (expression.includes("!")) {
+        expression = expression.replace(/(\d)!/g, (_, n) => factorial(parseInt(n)));
+    }
+    display.value = eval(expression);
+    } catch {
+        display.value = "Error";
+    }
+}
+
 function factorial(n) 
 {
     return n <= 1 ? 1 : n * factorial(n - 1);
 }
 
-function calculate()
+function toggleScientific() 
 {
-    try{
-        display.value = eval(display.value);
-    }catch{
-        display.value = "Error";
-    }
+    const sci = document.getElementById("sci");
+    sci.classList.toggle("show");
+
+    const toggleBtn = document.querySelector(".toggle-btn");
+    toggleBtn.innerText = sci.classList.contains("show") ? "▼ Other Operations": "▶ Other Operations";
 }
